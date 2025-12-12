e1792b631ce2ea75f3a1d49ef16f1f22d70cd1fb
Thanush-07 <thanushkumar722@gmail.com>
2025-12-12 23:08:45 +0530

    atlas to compass

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
-// server.js
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
 
-const authRoutes = require("./routes/auth");
-const companyRoutes = require("./routes/company");
+// server.js
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import companyRouter from "./routes/company.js";
+import authRouter from "./routes/auth.js";
+import institutionRouter from "./routes/institution.js";
+
+
+
+dotenv.config();
 
 const app = express();
 
@@ -13,7 +19,6 @@ app.use(cors());
 app.use(express.json());
 
 const MONGO_URL =
- // process.env.MONGO_URL ||"mongodb+srv://DTW:secret123@cluster0.vwhagph.mongodb.net/DTW?appName=Cluster0";
   process.env.MONGO_URL || "mongodb://127.0.0.1:27017/first-crop_db";
 
 mongoose
@@ -21,8 +26,9 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB error:", err));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/company", companyRoutes);
+app.use("/api/company", companyRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/institution", institutionRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
