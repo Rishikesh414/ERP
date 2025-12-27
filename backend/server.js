32fb43db8b6e0743eb72c8fab6b6cdc81fe03374
Thanush-07 <thanushkumar722@gmail.com>
2025-12-27 14:26:24 +0530

    student module error fixed

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const PORT = process.env.PORT || 5000;
 
 // -------------------- Middleware --------------------
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // -------------------- MongoDB Connection --------------------
 
